c6388608602dde7e31a61fe6c3e1960bbd266b58
Jon E Rankin <jr0436@bris.ac.uk>
2014-12-03 15:48:55 +0000

    uploaded photo to blobstore

--- wikieats.py
+++ wikieats.py
@@ -3,7 +3,8 @@ import urllib
 
 from google.appengine.api import users
 from google.appengine.ext import ndb
-
+from google.appengine.ext import blobstore
+from google.appengine.ext.webapp import blobstore_handlers
 import webapp2
 
 MAIN_PAGE_TEMPLATE = """\
@@ -115,10 +116,36 @@ class EditRestaurant(webapp2.RequestHandler):
 		self.response.write('<html><body>')
 		self.response.write(EDIT_RESTAURANT_TEMPLATE)
 
+class uploadPhotoPage(webapp2.RequestHandler):
+    def get(self):
+        upload_url = blobstore.create_upload_url('/upload')
+        self.response.out.write('<html><body>')
+        self.response.out.write('<form action="%s" method="POST" enctype="multipart/form-data">' % upload_url)
+        self.response.out.write("""Upload File: 
+                <input type="file" name="file"><br> <input type="submit"
+                name="submit" value="Submit"> </form></body></html>""")
+
+class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):
+    def post(self):
+        # 'file' is file upload field in the form
+        upload_files = self.get_uploads('file')
+        blob_info = upload_files[0]
+        self.redirect('/serve/%s' % blob_info.key())
+
+class ServeHandler(blobstore_handlers.BlobstoreDownloadHandler):
+    def get(self, resource):
+        resource = str(urllib.unquote(resource))
+        blob_info = blobstore.BlobInfo.get(resource)
+        self.send_blob(blob_info)
+
+
 application = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/addrestaurant', AddRestaurant),
 	('/postrestaurant', PostRestaurant),
 	('/displayrestaurant', MainPage),
 	('/editrestaurant', MainPage),
+    ('/uploadPhotoPage', uploadPhotoPage),
+    ('/upload', UploadHandler),
+    ('/serve/([^/]+)?', ServeHandler),
 ], debug=True)
