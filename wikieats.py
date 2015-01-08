3dc1abc7b591b9b18abe3078a0126e9098952a50
Jon E Rankin <jr0436@bris.ac.uk>
2015-01-08 01:22:48 +0000

    premerge

--- wikieats.py
+++ wikieats.py
@@ -436,16 +436,13 @@ NAV_2 = """
 			<input type="submit" value="GO" id="goButton">
       <script type="text/javascript">
         $('#goButton').hide();
-        console.log("helo");
         $('#city_link').on('change', function (e) {
           var optionSelected = $("option:selected", this);
           var text = optionSelected.text();
-          console.log(text);
           if(text == "Select City"){
           console.log("helo");
           $('#goButton').hide();
           }else{
-            console.log("#helssso");
             $('#goButton').show();
         }
       });
